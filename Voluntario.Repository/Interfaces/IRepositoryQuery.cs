1e9b32111f7e59691985c51906400e59bafba32a
leobower <leandro.fribeiro@grupofleury.com.br>
2019-11-27 19:01:12 -0300

    repository

---
+++ Voluntario.Repository/Interfaces/IRepositoryQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Voluntario.Repository.Interfaces
+{
+    interface IRepositoryQuery
+    {
+    }
+}
---
+++ Voluntario.Repository/Interfaces/IRepositoryWriter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Voluntario.Domain.Entities.Interfaces;
+
+namespace Voluntario.Repository.Interfaces
+{
+    public interface IRepositoryWriter : IDisposable
+    {
+        void Delete<T>(T voluntario) where T : class, IVoluntario, new();
+        void Update<T>(T voluntario) where T : class, IVoluntario, new();
+        void Add<T>(T voluntario) where T : class, IVoluntario, new();
+    }
+}
