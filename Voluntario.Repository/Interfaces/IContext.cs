0189423c3206325e8430bf579d1bc8cc53da1162
leobower <leandro.fribeiro@grupofleury.com.br>
2019-12-01 12:15:13 -0300

    ajuste

--- Voluntario.Repository/Interfaces/IContext.cs
+++ Voluntario.Repository/Interfaces/IContext.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Voluntario.Repository.Implementations
+namespace Voluntario.Repository.Interfaces
 {
-    public class MongoRepositoryWriter
+    public interface IContext
     {
 
     }
