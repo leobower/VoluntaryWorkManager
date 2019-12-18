330bc7c31bbf5fc90f9378e187e5e22e3026b5db
leobower <leandro.fribeiro@grupofleury.com.br>
2019-12-18 15:44:23 -0300

    Tracer

---
+++ CentralTracer/ITracerWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CentralTracer
+{
+    public interface ITracerWrapper : IDisposable
+    {
+        void TraceMessage(string format, params object[] args);
+    }
+}
