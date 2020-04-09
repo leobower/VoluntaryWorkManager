183c1f989986bb08b9aa95c68cd405378901976b
Leandro Ribeiro <33161307+leobower@users.noreply.github.com>
2020-04-09 01:06:58 -0300

    AJUSTES

---
+++ Voluntario.API.Rest/Controllers/VoluntarioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Voluntario.Domain.Entities.Interfaces;
+
+namespace Voluntario.API.Rest.Controllers
+{
+    public class VoluntarioController : Controller
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] IVoluntario value)
+        {
+            //TODO
+        }
+    }
+}
--- Voluntario.API.Rest/Startup.cs
+++ Voluntario.API.Rest/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Voluntario.API.Rest
 {
@@ -26,6 +27,9 @@ namespace Voluntario.API.Rest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger. Info { Title = "Voluntario API", Version = "V1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +44,10 @@ namespace Voluntario.API.Rest
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Voluntario API V1");
+            });
             app.UseHttpsRedirection();
             app.UseMvc();
         }
